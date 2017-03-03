c7ae6b268926ef062b9e77433fe01ea804fe7e71
Phil Morris <pmorris96@gmail.com>
2017-03-02 20:57:09 -0500

    Initial Commit

---
+++ Math Equations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Math Equations
+//
+//  Created by Phil Morris on 3/2/17.
+//  Copyright © 2017 Philip I Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
