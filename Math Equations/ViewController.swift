2b6969c5ce39c06bb6d7e175e698ecf959e8198d
Phil Morris <pmorris96@gmail.com>
2017-03-02 23:21:51 -0500

    Initial commit, most buttons are in place. Display is functional

--- Math Equations/ViewController.swift
+++ Math Equations/ViewController.swift
@@ -20,6 +20,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var display: UILabel!
+    var userIsTyping = false
+    var equationSectionCount = 0
+    
+    var displayValue: String {
+        get {
+            return display.text!
+        }
+        set {
+            display.text! = String(newValue)!
+        }
+    }
 
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTyping {
+            let textCurrentlyInDisplay = displayValue
+            displayValue = textCurrentlyInDisplay + digit
+        } else {
+            displayValue = digit
+            equationSectionCount += 1
+            userIsTyping = true
+        }
+
+    }
+    
+    @IBAction func touchOperation(_ sender: UIButton) {
+        if userIsTyping {
+            let symbol = sender.currentTitle!
+            if symbol == "=" {
+                equationSectionCount += 1
+            }
+            displayValue = displayValue + " " + symbol
+        }
+    }
+    
+    
+    @IBAction func touchClear(_ sender: UIButton) {
+        userIsTyping = false
+        equationSectionCount = 0
+        display.text = ""
+    }
+    
 }
 
